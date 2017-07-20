e361066f6260b7e3b3d301886c19c22798ec7e59
Dariusz <dariusz.lorenc@gmail.com>
2017-07-20 23:09:06 +1000

    Ribbon with chuck

---
+++ chuck-repo/src/main/kotlin/com/cloudnative/chuck/ChuckRepoApplication.kt
@@ -0,0 +1,27 @@
+package com.cloudnative.chuck
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@SpringBootApplication
+class ChuckRepoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ChuckRepoApplication::class.java, *args)
+}
+
+val random = Random()
+val jokes = arrayOf(
+        "Chuck Norris can kill two stones with one bird.",
+        "Chuck Norris counted to infinity. Twice.",
+        "Chuck Norris can hear sign language."
+)
+
+@RestController
+class ChuckController {
+    @GetMapping("/jokes/random")
+    fun joke() = jokes[random.nextInt(jokes.size)]
+}
---
+++ chuck-service/src/main/kotlin/com/cloudnative/chuck/ChuckServiceApplication.kt
@@ -0,0 +1,32 @@
+package com.cloudnative.chuck
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.cloud.netflix.ribbon.RibbonClient
+import org.springframework.context.annotation.Bean
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+
+@SpringBootApplication
+class ChuckServiceApplication {
+    @Bean
+    @LoadBalanced
+    fun restTemplate() = RestTemplate()
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ChuckServiceApplication::class.java, *args)
+}
+
+@RestController
+class ChuckController(val chuckRepoClient: ChuckRepoClient) {
+    @GetMapping("/joke")
+    fun joke() = chuckRepoClient.joke()
+}
+
+@RibbonClient("chuck-repo")
+class ChuckRepoClient(val restTemplate: RestTemplate) {
+    fun joke() = restTemplate.getForObject("http://chuck-repo/jokes/random", String::class.java)
+}
